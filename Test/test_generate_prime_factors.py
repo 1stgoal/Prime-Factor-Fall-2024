3e42a30d0dcf010df375c8319d1f8f96c7cf7a67
Gurinder Singh <92890724+1stgoal@users.noreply.github.com>
2025-02-10 18:04:05 -0500

    initial commit

--- Test/test_generate_prime_factors.py
+++ Test/test_generate_prime_factors.py
@@ -1,1 +1,6 @@
 
+import pytest
+import generate_prime_factors from prime
+def test_valuerror():
+    with pytest.raises(ValueError):
+        generate_prime_factors ("0")
--- prime.py
+++ prime.py
@@ -1,3 +1,2 @@
-"""
-prime.py -- Write the application code here
-"""
+def generate_prime_factors():
+    pass
