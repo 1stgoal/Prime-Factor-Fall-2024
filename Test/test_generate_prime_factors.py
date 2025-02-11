630ab6c4d8e945ceb9356e18bb6600042b10fdf0
Gurinder Singh <92890724+1stgoal@users.noreply.github.com>
2025-02-11 09:27:53 -0500

    first test failed

--- Test/test_generate_prime_factors.py
+++ Test/test_generate_prime_factors.py
@@ -1,6 +1,9 @@
 
 import pytest
 from prime import generate_prime_factors
+
+
+
 def test_valuerror():
     with pytest.raises(ValueError):
-        generate_prime_factors ("0")
+        generate_prime_factors(0)
