b7c914ff31994ba6f8e7c89c077a663022094d23
Gurinder Singh <92890724+1stgoal@users.noreply.github.com>
2025-02-11 09:32:26 -0500

    first test passes

---
+++ prime.py
@@ -0,0 +1,8 @@
+def generate_prime_factors(user_input):
+    if user_input > 1:
+        return user_input  # Return the number if it is greater than 1
+    else:
+        raise ValueError
+
+
+print(generate_prime_factors(15))
