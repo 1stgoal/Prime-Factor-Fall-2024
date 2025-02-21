58062e61b49c11957c7f5c976e53900be92a5ab1
Gurinder Singh <92890724+1stgoal@users.noreply.github.com>
2025-02-21 17:21:35 -0500

    Step1
    Wrote the test that asserts that when generate_prime_factors is called with a data type that is not an integer (e.g. a string or float), a ValueError is raised. Also added the comments

--- Test/test_generate_prime_factors.py
+++ Test/test_generate_prime_factors.py
@@ -1,16 +1,17 @@
 import pytest
 import sys
 import os
+# Letting the test program know where the program being tested is located (path)
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from prime import generate_prime_factors
 
-
+#testing the string input
 def test_string():
     with pytest.raises(ValueError):
         generate_prime_factors("f")
 
-
+#testing the float input
 def test_floatinput():
     with pytest.raises(ValueError):
         generate_prime_factors(.5)
